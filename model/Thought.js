9fe499e633bba152c1be5f6f5d25173277bccf93
Haozhe-H <haozhehuang@outlook.com>
2023-05-03 16:55:14 -0400

    update thought model

---
+++ model/Thought.js
@@ -0,0 +1,76 @@
+const { Schema, model, Types } = require("mongoose");
+
+//reaction
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toISOString(),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// thought
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toISOString(),
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get count of friends
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// create thought model
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
