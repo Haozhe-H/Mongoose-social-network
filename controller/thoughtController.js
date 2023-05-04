d4dcad186a9ad8aa1141b2ef202535e45b319e65
Haozhe-H <haozhehuang@outlook.com>
2023-05-03 23:44:12 -0400

    update post and put controller for thought

--- controller/thoughtController.js
+++ controller/thoughtController.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require("../model");
 
+// get thought, all and by id
 function getThought(req, res) {
   Thought.find({})
     .then((thought) => res.json(thought))
@@ -11,8 +12,41 @@ function getSingleThought(req, res) {
     .select("-__v")
     .then((thought) =>
       !thought
-        ? res.status(404).json({ message: "No Thought find with this ID!" })
+        ? res.status(404).json({ message: "No Thought find with this ID." })
         : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+}
+
+// create thought
+function createThought(req, res) {
+  Thought.create(req.body)
+    .then(({ _id }) => {
+      return User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: _id } },
+        { new: true }
+      );
+    })
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: "No User find with this ID." })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+}
+
+// update
+function updateThought(req, res) {
+  Thought.findOneAndUpdate(
+    { _id: req.params.thoughtId },
+    { $set: req.body },
+    { runValidators: true, New: true }
+  )
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: "No thought find with this ID." })
+        : res.json(user)
     )
     .catch((err) => res.status(500).json(err));
 }
