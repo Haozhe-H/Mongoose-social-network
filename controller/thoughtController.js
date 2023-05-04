1e9750fdb98d655fd20655be2aad41c9545f5f77
Haozhe-H <haozhehuang@outlook.com>
2023-05-04 00:04:04 -0400

    update delete thought route

--- controller/thoughtController.js
+++ controller/thoughtController.js
@@ -50,3 +50,25 @@ function updateThought(req, res) {
     )
     .catch((err) => res.status(500).json(err));
 }
+
+// delete
+function deleteThought(req, res) {
+  Thought.findOneAndDelete({ _id: req.params.thoughtId })
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: "No thought find with this ID." })
+        : User.findOneAndUpdate(
+            { thoughts: req.params.thoughtId },
+            { $pull: { thoughts: req.params.thoughtId } },
+            { new: true }
+          )
+    )
+    .then((user) =>
+      !user
+        ? res
+            .status(404)
+            .json({ message: "Thought deleted, no corresponding user found." })
+        : res.json({ message: "Thought has been deleted." })
+    )
+    .catch((err) => res.status(500).json(err));
+}
